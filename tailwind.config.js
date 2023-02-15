16e290cb5ae839ac0572aa03301e230ceca86493
b4cktr4ck5r3 <flennertom@gmail.com>
2023-02-14 21:05:00 -0500

    Add Tailwind + remove unused files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+/* eslint-env node */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./pages/**/*.{vue,js,ts,jsx,tsx}",
+    "./components/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

241f8851520dc1aa449dd9cd08c533c5feafa4cf
b4cktr4ck5r3 <flennertom@gmail.com>
2023-02-15 16:17:24 -0500

    upgrade dropdown component

--- src/components/MultistepFormDropdown.vue
+++ src/components/MultistepFormDropdown.vue
@@ -1,58 +1,100 @@
-<script setup lang="ts">
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
-import { ChevronDownIcon } from "@heroicons/vue/20/solid";
-import "/node_modules/flag-icons/css/flag-icons.min.css";
-</script>
-
 <template>
-  <Menu as="div" class="relative inline-block text-left w-full">
-    <div>
-      <MenuButton
-        class="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100"
-      >
-        Pays
-        <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
-      </MenuButton>
-    </div>
-
-    <transition
-      enter-active-class="transition ease-out duration-100"
-      enter-from-class="transform opacity-0 scale-95"
-      enter-to-class="transform opacity-100 scale-100"
-      leave-active-class="transition ease-in duration-75"
-      leave-from-class="transform opacity-100 scale-100"
-      leave-to-class="transform opacity-0 scale-95"
+  <Listbox as="div" v-model="selected">
+    <ListboxLabel class="block text-sm font-medium text-gray-700"
+      >Pays</ListboxLabel
     >
-      <MenuItems
-        class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+    <div class="relative mt-1">
+      <ListboxButton
+        class="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
       >
-        <div class="py-1">
-          <MenuItem v-slot="{ active }">
-            <a
-              href="#"
+        <span class="flex items-center">
+          <span
+            :class="selected.icon + ' h-6 w-6 flex-shrink-0 rounded-full'"
+          ></span>
+          <span class="ml-3 block truncate">{{ selected.name }}</span>
+        </span>
+        <span
+          class="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2"
+        >
+          <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+        </span>
+      </ListboxButton>
+
+      <transition
+        leave-active-class="transition ease-in duration-100"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
+      >
+        <ListboxOptions
+          class="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+        >
+          <ListboxOption
+            as="template"
+            v-for="country in countries"
+            :key="country.id"
+            :value="country"
+            v-slot="{ active, selected }"
+          >
+            <li
               :class="[
-                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                'block px-4 py-2 text-sm',
+                active ? 'text-white bg-indigo-600' : 'text-gray-900',
+                'relative cursor-default select-none py-2 pl-3 pr-9',
               ]"
             >
-              <i class="fi fi-ca"></i>
-              Canada
-            </a>
-          </MenuItem>
-          <MenuItem v-slot="{ active }">
-            <a
-              href="#"
-              :class="[
-                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                'block px-4 py-2 text-sm',
-              ]"
-            >
-              <i class="fi fi-us"></i>
-              Etats-Unis
-            </a>
-          </MenuItem>
-        </div>
-      </MenuItems>
-    </transition>
-  </Menu>
+              <div class="flex items-center">
+                <span
+                  :class="country.icon + ' h-6 w-6 flex-shrink-0 rounded-full'"
+                ></span>
+                <span
+                  :class="[
+                    selected ? 'font-semibold' : 'font-normal',
+                    'ml-3 block truncate',
+                  ]"
+                  >{{ country.name }}</span
+                >
+              </div>
+
+              <span
+                v-if="selected"
+                :class="[
+                  active ? 'text-white' : 'text-indigo-600',
+                  'absolute inset-y-0 right-0 flex items-center pr-4',
+                ]"
+              >
+                <CheckIcon class="h-5 w-5" aria-hidden="true" />
+              </span>
+            </li>
+          </ListboxOption>
+        </ListboxOptions>
+      </transition>
+    </div>
+  </Listbox>
 </template>
+
+<script setup lang="ts">
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+import { ref } from "vue";
+import {
+  Listbox,
+  ListboxButton,
+  ListboxLabel,
+  ListboxOption,
+  ListboxOptions,
+} from "@headlessui/vue";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/vue/20/solid";
+
+const countries = [
+  {
+    id: 0,
+    name: "Canada",
+    icon: "fi fi-ca",
+  },
+  {
+    id: 1,
+    name: "Etats-Unis",
+    icon: "fi fi-us",
+  },
+];
+
+const selected = ref(countries[0]);
+</script>
